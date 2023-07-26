6407cb99fbf5149071dfb0218b7d717cdcea4dcd
frica12 <fricaa@naver.com>
2023-07-26 19:52:50 +0900

    sh branch

--- week4_project/src/screens/Tabs/FriendScreen.js
+++ week4_project/src/screens/Tabs/FriendScreen.js
@@ -1,27 +1,121 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
-import "react-native-get-random-values";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Image,
+  SafeAreaView,
+} from "react-native";
 
 function FriendScreen({ userInfo }) {
-  console.log(userInfo);
+  const [users, setUsers] = useState([]);
 
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_server_uri}/api/get_all_users`)
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const renderItem = ({ item }) => {
+    const formattedPhoneNumber = item.phone_number.replace(
+      /(\d{3})(\d{4})(\d{4})/,
+      "$1-$2-$3"
+    );
+
+    return (
+      <View style={styles.userContainer}>
+        <Image style={styles.profileImage} source={{ uri: item.profileURL }} />
+        <View>
+          <Text style={styles.userName}>{item.name}</Text>
+          <Text style={styles.userPhone}>{formattedPhoneNumber}</Text>
+        </View>
+        <Text style={styles.stateMessage}>{item.stateMessage}</Text>
+      </View>
+    );
+  };
   return (
-    <View style={styles.container}>
-      <Text style={styles.texts}>친구 목록 페이지</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.profile_title}>친구 목록</Text>
+      <View style={styles.separator} />
+      <FlatList
+        contentContainerStyle={styles.list}
+        data={users}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+        showsVerticalScrollIndicator={false}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     backgroundColor: "white",
+  },
+  profile_title: {
+    padding: 20,
+    alignItems: "flex-start",
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  title: {
+    fontSize: 30,
+    marginTop: 50,
+    marginBottom: 15,
+    color: "#2196F3",
+  },
+
+  userContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+    borderRadius: 10,
+    padding: 10,
+    marginVertical: 5,
+    width: 350,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  list: {
     alignItems: "center",
   },
-  texts: {
-    fontSize: 25,
-    justifyContent: "center",
+  userContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+    borderRadius: 10,
+    padding: 10,
+    marginVertical: 5,
+    width: 400,
+  },
+  separator: {
+    width: "100%",
+    height: 1,
+    backgroundColor: "lightgray",
+    marginBottom: 10,
+  },
+  profileImage: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 20,
+  },
+  userName: {
+    fontSize: 20,
+    marginBottom: 5,
+  },
+  userPhone: {
+    fontSize: 14,
+    color: "gray",
+  },
+  stateMessage: {
+    fontSize: 15,
+    marginLeft: 20,
   },
 });
 
--- week4_project/src/screens/Tabs/MypageScreen.js
+++ week4_project/src/screens/Tabs/MypageScreen.js
@@ -13,9 +13,38 @@ import {
 } from "react-native";
 
 function MypageScreen({ userInfo }) {
-  console.log(userInfo);
-
   const [name, setName] = useState("");
+  const [stateMessage, setStateMessage] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalVisible2, setModalVisible2] = useState(false);
+
+  const [pastAppointments, setPastAppointments] = useState([]);
+  const [selectedAppointment, setSelectedAppointment] = useState(null);
+  const [membersNames, setMembersNames] = useState([]);
+
+  const openAppointmentDetail = (appointment) => {
+    setSelectedAppointment(appointment);
+    setModalVisible2(true);
+  };
+
+  useEffect(() => {
+    async function fetchPastAppointments() {
+      const response = await fetch(
+        `${process.env.REACT_APP_server_uri}/api/past_appointments?id=${userInfo.id}`
+      );
+      const pastAppointmentsData = await response.json();
+      setPastAppointments(pastAppointmentsData.data || []);
+    }
+
+    fetchPastAppointments();
+  }, []);
+
+  const openPastAppointment = () => {
+    if (pastAppointments.length > 0) {
+      setSelectedAppointment(pastAppointments[0]);
+      setModalVisible2(true);
+    }
+  };
 
   useEffect(() => {
     async function fetchName() {
@@ -28,6 +57,60 @@ function MypageScreen({ userInfo }) {
 
     fetchName();
   }, []);
+
+  useEffect(() => {
+    async function fetchStateMessage() {
+      const response = await fetch(
+        `${process.env.REACT_APP_server_uri}/api/get_user_state_message?id=${userInfo.id}`
+      );
+      const userData = await response.json();
+      setStateMessage(userData.stateMessage || "");
+    }
+
+    fetchStateMessage();
+  }, []);
+
+  const updateStateMessage = async () => {
+    console.log(userInfo.id);
+    const response = await fetch(
+      `${process.env.REACT_APP_server_uri}/api/update_state_message`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: userInfo.id,
+          stateMessage: stateMessage,
+        }),
+      }
+    );
+    if (response.ok) {
+      setModalVisible(false);
+    }
+  };
+
+  const fetchUserNameById = async (userId) => {
+    const response = await fetch(
+      `${process.env.REACT_APP_server_uri}/api/get_username_byid?id=${userId}`
+    );
+    const userData = await response.json();
+    return userData.name;
+  };
+
+  useEffect(() => {
+    if (selectedAppointment) {
+      const fetchMembersNames = async () => {
+        const names = await Promise.all(
+          selectedAppointment.members.map((memberId) =>
+            fetchUserNameById(memberId)
+          )
+        );
+        setMembersNames(names);
+      };
+      fetchMembersNames();
+    }
+  }, [selectedAppointment]);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -49,8 +132,115 @@ function MypageScreen({ userInfo }) {
             <Text style={styles.text2}>{"이메일"}</Text>
             <Text style={styles.text3}>{userInfo.kakao_account.email}</Text>
           </View>
+          <View style={styles.information_row}>
+            <Text style={styles.text2}>{"상태 메시지"}</Text>
+            <Text style={styles.text3}>{stateMessage}</Text>
+          </View>
+          <View style={styles.btbt}>
+            <TouchableOpacity
+              style={styles.btstyle}
+              onPress={() => setModalVisible(true)}
+            >
+              <Text style={styles.buttonText}>상태 메시지 변경</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.separator2} />
+        </View>
+        <View>
+          <Text style={styles.modalText2}>이전 약속</Text>
+        </View>
+        <View>
+          {pastAppointments.map((appointment, index) => (
+            <TouchableOpacity
+              key={index}
+              onPress={() => openAppointmentDetail(appointment)}
+            >
+              <Text style={styles.appointmentText}>
+                {new Date(appointment.times).toLocaleString("ko-KR", {
+                  year: "numeric",
+                  month: "2-digit",
+                  day: "2-digit",
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+                {" - " + appointment.place}
+              </Text>
+            </TouchableOpacity>
+          ))}
         </View>
       </ScrollView>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>상태 메시지 변경</Text>
+            <TextInput
+              style={styles.textinputView}
+              value={stateMessage}
+              onChangeText={setStateMessage}
+            />
+            <View style={styles.buttonContainer}>
+              <Button title="완료" onPress={updateStateMessage} />
+              <Button title="취소" onPress={() => setModalVisible(false)} />
+            </View>
+          </View>
+        </View>
+      </Modal>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible2}
+        onRequestClose={() => {
+          setModalVisible2(!modalVisible2);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            {selectedAppointment ? (
+              <View>
+                {membersNames.map((name, index) => (
+                  <Text key={index} style={styles.text}>
+                    멤버 {index + 1}: {name}
+                  </Text>
+                ))}
+                <Text style={styles.text}>
+                  일시:{" "}
+                  {new Date(selectedAppointment.times).toLocaleString("ko-KR", {
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    second: "2-digit",
+                  })}
+                </Text>
+
+                <Text style={styles.text}>
+                  장소: {selectedAppointment.place}
+                </Text>
+                <Text style={styles.text}>
+                  내용: {selectedAppointment.content}
+                </Text>
+                <Text style={styles.text}>
+                  위치:{" "}
+                  {`위도: ${selectedAppointment.location.latitude}, 경도: ${selectedAppointment.location.longitude}`}
+                </Text>
+              </View>
+            ) : null}
+            <View style={styles.buttonContainer}>
+              <Button title="닫기" onPress={() => setModalVisible2(false)} />
+            </View>
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 }
@@ -75,6 +265,12 @@ const styles = StyleSheet.create({
     padding: 20,
     alignItems: "flex-start",
     fontSize: 25,
+    fontWeight: "bold",
+  },
+  modalText2: {
+    fontSize: 20,
+    marginBottom: 20,
+    textAlign: "center",
     fontWeight: "bold",
   },
   information_row: {
@@ -113,6 +309,13 @@ const styles = StyleSheet.create({
     height: 1,
     backgroundColor: "lightgray",
   },
+  separator2: {
+    width: "100%",
+    height: 1,
+    backgroundColor: "lightgray",
+    marginTop: 20,
+    marginBottom: 20,
+  },
   centeredView: {
     flex: 1,
     justifyContent: "center",
@@ -150,8 +353,37 @@ const styles = StyleSheet.create({
     fontSize: 16,
     textAlign: "center",
   },
-  textinputView: {
+  appointmentText: {
+    textAlign: "left",
+    marginHorizontal: 40,
+    marginBottom: 15,
     fontSize: 20,
+    color: "gray",
+  },
+  textinputView: {
+    marginTop: 15,
+    fontSize: 30,
+    marginBottom: -5,
+  },
+  btstyle: {
+    marginTop: 20,
+    backgroundColor: "#2196F3",
+    padding: 10,
+    borderRadius: 5,
+    width: "70%",
+    alignContent: "center",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    textAlign: "center",
+  },
+  btbt: {
+    alignContent: "center",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
